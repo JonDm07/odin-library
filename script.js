b27129c26a1e7c1fd12e44db03000c4d98943d53
JonDm07 <jan.drmota1@gmail.com>
2023-02-17 23:15:14 +0100

    Associate book object with it's card

--- script.js
+++ script.js
@@ -2,39 +2,43 @@ let myLibrary = [
 
 ]
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, indexOfCard) {
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read
+    this.read = read,
+    this.indexOfCard = indexOfCard
 }
-myLibrary.push(new Book("a", "b", 45, "c"))
 
 let createDiv = function() {
     let container = document.querySelector(".container")
     let div = document.createElement("div")
+
+
     div.classList.add("book-card")
     container.appendChild(div)
+    
+
     for(let i = 0; i < 4; i++) {
         let p = document.createElement("p")
         div.appendChild(p)
     }
+
+    for(let i = 0; i < 2; i++) {
+        let button = document.createElement("button")
+        button.classList.add("book-card-button")
+        div.appendChild(button)
+    }
+    let buttons = document.querySelectorAll(".book-card-button")
+
+    buttons[0].textContent = "Read"
+    buttons[1].textContent = "Delete"
+    
 }
 
-document.querySelector(".addButt").addEventListener("click", () => {
-        let title = prompt("Title:")
-        let author = prompt("Author:")
-        let pages = prompt("Pages:")
-        myLibrary.push(new Book(title, author, pages))
-        createDiv()
-        let paragraphs = document.querySelectorAll(".container .book-card:last-child p")
-        paragraphs[0].textContent = myLibrary[myLibrary.length - 1].title
-        paragraphs[1].textContent = myLibrary[myLibrary.length - 1].author
-        paragraphs[2].textContent = myLibrary[myLibrary.length - 1].pages
-})
-
-document.querySelector(".toggle").addEventListener("click", () => {
+document.querySelector(".addNewBook").addEventListener("click", () => {
     let formDiv = document.querySelector(".form")
+
     if(formDiv.style.display === "grid") {
         formDiv.style.display = "none"
     } else {
@@ -42,19 +46,35 @@ document.querySelector(".toggle").addEventListener("click", () => {
     }
 })
 
+let numOfCards = 0
+
 document.querySelector("#submit").addEventListener("click", (e) => { 
-    e.preventDefault();  
+    e.preventDefault();
+    createDiv()
+    
+    let div = document.querySelector(".book-card:last-of-type")
+    div.setAttribute("data-num", numOfCards)
+
+    
+    let paragraphs = document.querySelectorAll(".container .book-card:last-child p")
     let inputs = document.querySelectorAll(".form label input")
     let title = inputs[0].value
     let author = inputs[1].value
     let pages = inputs[2].value
-    myLibrary.push(new Book(title, author, pages))
-    createDiv()
-    let paragraphs = document.querySelectorAll(".container .book-card:last-child p")
+    let read;   
+
+    if (inputs[3].checked === true) {
+        read = "Yes"
+    } else  {
+        read = "No"
+    }
+
+    myLibrary.push(new Book(title, author, pages, read, numOfCards))
+
     paragraphs[0].textContent = myLibrary[myLibrary.length - 1].title
     paragraphs[1].textContent = myLibrary[myLibrary.length - 1].author
     paragraphs[2].textContent = myLibrary[myLibrary.length - 1].pages
+    paragraphs[3].textContent = myLibrary[myLibrary.length - 1].read
+
+    numOfCards++
 })
-
-
-
