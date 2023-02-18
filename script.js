448c16b6d856fe18146810b0c0abf38d3fe091a3
JonDm07 <jan.drmota1@gmail.com>
2023-02-18 18:42:20 +0100

    Add working delete button, Fix bugs

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let myLibrary = [
-
-]
+let myLibrary = []
+let deleteButtons;
 
 function Book(title, author, pages, read, indexOfCard) {
     this.title = title,
@@ -8,6 +7,11 @@ function Book(title, author, pages, read, indexOfCard) {
     this.pages = pages,
     this.read = read,
     this.indexOfCard = indexOfCard
+}
+
+function getDeleteButtons() {
+    deleteButtons = document.querySelectorAll(".delete")
+
 }
 
 let createDiv = function() {
@@ -33,26 +37,21 @@ let createDiv = function() {
 }
 
 document.querySelector(".addNewBook").addEventListener("click", () => {
-    let formDiv = document.querySelector(".form")
+    let form = document.querySelector(".form")
+    let formDiv = document.querySelector(".form-div")
 
-    if(formDiv.style.display === "grid") {
-        formDiv.style.display = "none"
+    if(form.style.display === "grid") {
+        form.style.display = "none"
+        formDiv.style.display = "none"        
     } else {
+        form.style.display = "grid"
         formDiv.style.display = "grid"
     }
 })
 
-let numOfCards = 0
-
 document.querySelector("#submit").addEventListener("click", (e) => { 
     e.preventDefault();
     createDiv()
-    
-    let div = document.querySelector(".book-card:last-of-type")
-    div.setAttribute("data-num", numOfCards)
-
-    let deleteButton = document.querySelector(".book-card:last-of-type .book-card-button:last-of-type")
-    deleteButton.setAttribute("data-num", numOfCards)
     
     let paragraphs = document.querySelectorAll(".container .book-card:last-child p")
     let inputs = document.querySelectorAll(".form label input")
@@ -67,12 +66,53 @@ document.querySelector("#submit").addEventListener("click", (e) => {
         read = "No"
     }
 
-    myLibrary.push(new Book(title, author, pages, read, numOfCards))
+    myLibrary.push(new Book(title, author, pages, read,))
+    myLibrary[myLibrary.length - 1].index = myLibrary.length - 1
+
+    let deleteButton = document.querySelector(".book-card:last-of-type .book-card-button:last-of-type")
+    deleteButton.classList.add("delete")
+    deleteButton.setAttribute("data-num", myLibrary.length - 1)
+
+    let div = document.querySelector(".book-card:last-of-type")
+    div.setAttribute("data-num", myLibrary.length - 1)
 
     paragraphs[0].textContent = myLibrary[myLibrary.length - 1].title
     paragraphs[1].textContent = myLibrary[myLibrary.length - 1].author
     paragraphs[2].textContent = myLibrary[myLibrary.length - 1].pages
     paragraphs[3].textContent = myLibrary[myLibrary.length - 1].read
 
-    numOfCards++
+    getDeleteButtons();
+
+    deleteButtons.forEach(button => {
+        button.onclick = () => {
+            let cards = document.querySelectorAll(".book-card")
+            let delButtons = document.querySelectorAll(".delete")
+            for(let i = 0; i < cards.length; i++) {
+                if( button.getAttribute("data-num") === cards[i].getAttribute("data-num")) {
+                    cards[i].remove()
+                    myLibrary.splice(i, 1)
+                    for(let j = 0; j < cards.length; j++) {
+                        cards = document.querySelectorAll(".book-card")
+                        delButtons = document.querySelectorAll(".delete")
+                        cards[j].setAttribute("data-num", j)
+                        delButtons[j].setAttribute("data-num", j)
+
+                    }
+
+                }
+            }
+        }
+    })
+
 })
+
+
+
+
+
+
+
+
+
+
+
