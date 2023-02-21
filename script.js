6ac5bd9f4b734bcdd71465c9eab9b6123a25b399
JonDm07 <jan.drmota1@gmail.com>
2023-02-21 19:14:35 +0100

    Add function to the read button

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function getDeleteButtons() {
 
 }
 
+
 let createDiv = function() {
     let container = document.querySelector(".container")
     let div = document.createElement("div")
@@ -76,12 +77,17 @@ document.querySelector("#submit").addEventListener("click", (e) => {
     myLibrary.push(new Book(title, author, pages, read,))
     myLibrary[myLibrary.length - 1].index = myLibrary.length - 1
 
+    let readButton = document.querySelector(".book-card:last-of-type .book-card-button:first-of-type")
+    readButton.classList.add("read")
+    readButton.setAttribute("data-num", myLibrary.length - 1)
+
     let deleteButton = document.querySelector(".book-card:last-of-type .book-card-button:last-of-type")
     deleteButton.classList.add("delete")
     deleteButton.setAttribute("data-num", myLibrary.length - 1)
 
     let div = document.querySelector(".book-card:last-of-type")
     div.setAttribute("data-num", myLibrary.length - 1)
+    div.setAttribute("data-read", myLibrary[myLibrary.length - 1].read)
 
     paragraphs[0].textContent = myLibrary[myLibrary.length - 1].title
     paragraphs[1].textContent = myLibrary[myLibrary.length - 1].author
@@ -94,6 +100,7 @@ document.querySelector("#submit").addEventListener("click", (e) => {
         button.onclick = () => {
             let cards = document.querySelectorAll(".book-card")
             let delButtons = document.querySelectorAll(".delete")
+            let readButtons = document.querySelectorAll(".read")
             for(let i = 0; i < cards.length; i++) {
                 if( button.getAttribute("data-num") === cards[i].getAttribute("data-num")) {
                     cards[i].remove()
@@ -101,10 +108,36 @@ document.querySelector("#submit").addEventListener("click", (e) => {
                     for(let j = 0; j < cards.length; j++) {
                         cards = document.querySelectorAll(".book-card")
                         delButtons = document.querySelectorAll(".delete")
+                        readButtons = document.querySelectorAll(".read")
                         cards[j].setAttribute("data-num", j)
                         delButtons[j].setAttribute("data-num", j)
+                        readButtons[j].setAttribute("data-num", j)
 
                     }
+                }
+            }
+        }
+    })
+
+    let readButtons = document.querySelectorAll(".read")
+
+    readButtons.forEach(button => {
+        button.onclick = () => {
+            let cards = Array.from(document.querySelectorAll(".book-card"))
+            let readPara = Array.from(document.querySelectorAll(".book-card p:nth-of-type(4)"))
+            for(let i = 0; i < myLibrary.length; i++) {
+                if(button.getAttribute("data-num") === cards[i].getAttribute("data-num")) {
+                    if(myLibrary[i].read === "Yes") {
+                        myLibrary[i].read = "No"
+                        if (cards.indexOf(cards[i] === readPara.indexOf(readPara[i]))) {
+                            readPara[i].textContent = "No"
+                        }
+                    } else {
+                        myLibrary[i].read = "Yes"
+                        cards[i].setAttribute("data-read", "Yes")
+                        if (cards.indexOf(cards[i] === readPara.indexOf(readPara[i]))) {
+                            readPara[i].textContent = "Yes"
+                        }}
                 }
             }
         }
